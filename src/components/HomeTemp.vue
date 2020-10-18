ab84c70222818d9e1ba898d4a346e2bfc6ddc2a6
Yke ten Donkelaar <y.tendonkelaar@code14.nl>
2020-10-18 21:29:38 +0200

    fix buy buttons

--- src/components/HomeTemp.vue
+++ src/components/HomeTemp.vue
@@ -81,6 +81,9 @@
         <!-- signed ber 2021 / print -->
         <div id='product-component-1602178988244'></div>
 
+        <!-- signed ams 2021 / print -->
+        <div id='product-component-1603049171094'></div>
+
         <!-- 2020 -->
         <div id='product-component-1571344510124'></div>
 
@@ -576,6 +579,92 @@
             });
         }
     })();
+
+    //ams 2021 - calendar & print
+    (function () {
+        var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+        if (window.ShopifyBuy) {
+            if (window.ShopifyBuy.UI) {
+                ShopifyBuyInit();
+            } else {
+                loadScript();
+            }
+        } else {
+            loadScript();
+        }
+        function loadScript() {
+            var script = document.createElement('script');
+            script.async = true;
+            script.src = scriptURL;
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+            script.onload = ShopifyBuyInit;
+        }
+        function ShopifyBuyInit() {
+            var client = ShopifyBuy.buildClient({
+                domain: 'de-amsterdamse-naaktkalender.myshopify.com',
+                storefrontAccessToken: '654e8e16671673a9936fe0afffe19306',
+            });
+            ShopifyBuy.UI.onReady(client).then(function (ui) {
+                ui.createComponent('product', {
+                    id: '4701905518658',
+                    node: document.getElementById('product-component-1603049171094'),
+                    moneyFormat: '%E2%82%AC%7B%7Bamount_with_comma_separator%7D%7D',
+                    options: {
+                        "product": {
+                            "styles": {
+                                "product": {
+                                    "@media (min-width: 601px)": {
+                                        "max-width": "calc(25% - 20px)",
+                                        "margin-left": "20px",
+                                        "margin-bottom": "50px"
+                                    }
+                                }
+                            },
+                            "buttonDestination": "checkout",
+                            "text": {
+                                "button": "Buy now"
+                            }
+                        },
+                        "productSet": {
+                            "styles": {
+                                "products": {
+                                    "@media (min-width: 601px)": {
+                                        "margin-left": "-20px"
+                                    }
+                                }
+                            }
+                        },
+                        "modalProduct": {
+                            "contents": {
+                                "img": false,
+                                "imgWithCarousel": true,
+                                "button": false,
+                                "buttonWithQuantity": true
+                            },
+                            "styles": {
+                                "product": {
+                                    "@media (min-width: 601px)": {
+                                        "max-width": "100%",
+                                        "margin-left": "0px",
+                                        "margin-bottom": "0px"
+                                    }
+                                }
+                            },
+                            "text": {
+                                "button": "Add to cart"
+                            }
+                        },
+                        "cart": {
+                            "text": {
+                                "total": "Subtotal",
+                                "button": "Checkout"
+                            }
+                        }
+                    },
+                });
+            });
+        }
+    })();
 </script>
 
 <style>
@@ -637,8 +726,9 @@
     #product-component-1571344433543,
     #product-component-1602178881523,
     #product-component-1602178731128,
-    #product-component-1602178988244 {
-        display: true;
+    #product-component-1602178988244,
+    #product-component-1603049171094{
+        display: none;
         z-index: 100;
     }
 
